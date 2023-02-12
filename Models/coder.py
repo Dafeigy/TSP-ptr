9ba162139be4971a99917207573e0b093bf1879e
Dafeigy <1070642765@qq.com>
2023-02-12 19:50:51 +0800

    <Add>:data_generator

--- Models/coder.py
+++
@@ -1,6 +0,0 @@
-import torch.nn as nn
-
-class Encoder(nn.Module):
-    def __init__(self) -> None:
-        super(Encoder, self).__init__()
-        
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import itertools
 import numpy as np
 import math
 
-
+# For Inference
 def generate_cities(N)->list[int,int]:
     '''
     Generate N cities location [(x1,y1),...(xn,yn)]
@@ -13,7 +13,7 @@ def generate_cities(N)->list[int,int]:
 
 def calculate_distance(p1,p2,sqrt=False)->float:
     '''
-    Calculate distance between p1,p2
+    Calculate distance between p1,p2, set False on sqrt for faster calcuation.
     '''
     if sqrt:
         return math.sqrt(sum([(x - y) ** 2 for x, y in zip(p1, p2)]))
@@ -22,16 +22,38 @@ def calculate_distance(p1,p2,sqrt=False)->float:
 
 def calculate_total_dis(cities:list[tuple], order:list):
     '''
-    Calculate
+    Calculate total distance^2 as loss function
     '''
     return sum([calculate_distance(cities[order[i]], cities[order[(i+1)%len(order)]]) for i in range(len(order))])
 
 
+# For Training
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.autograd as autograd
+from torch.utils.data import Dataset, dataloader
+
+class TSPdataset(Dataset):
+    '''
+    Generate `num_samples` of `num_nodes` cities.
+    '''
+    def __init__(self, num_nodes, num_samples) -> None:
+        super(TSPdataset).__init__()
+        self.data_set = []
+
+        for _ in tqdm(range(num_samples)):
+            x = torch.FloatTensor(2, num_nodes).uniform_(0, 1)
+            self.data_set.append(x)
+        self.size = len(self.data_set)
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, index):
+        return self.data_set[index]
+
 if __name__ == '__main__':
-    a = generate_cities(3)
-    order = [0,1,2]
-    test = calculate_distance((1,1),(2,2))
-    print(test)
-    print(a)
-    loss = calculate_total_dis(a,order)
-    print(loss)
+    trainset = TSPdataset(10,1000)
+    print(torch.FloatTensor(2, 10).uniform_(0, 6))
